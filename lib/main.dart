6f65c8e2191ad718b4584584d7e0bc6211c5cb31
Ebisa Girma <ebisagirma41@gmail.com>
2024-10-03 13:00:17 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+      title: const Text('Coffe Card'),
+      backgroundColor: Colors.brown[700],
+      centerTitle: true,
+    ),
+    body: const Text('Hello World'),
+  )));
+}
